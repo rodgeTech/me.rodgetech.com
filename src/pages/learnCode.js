f07e8e3eb5ef9d8fc40873eeca797ff28736f846
Luis Rodriguez <luisrodge@outlook.com>
2021-09-12 14:17:31 -0600

    Minor update

--- src/pages/learnCode.js
+++ src/pages/learnCode.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { ContainerLayout, Title, Intro } from "../components/common"
-import { Text } from "../components/about/style"
+import { ContainerLayout, Intro } from "../components/common"
+import { Text, Title } from "../components/about/style"
 
 const LearnCodePage = () => (
   <Layout>
